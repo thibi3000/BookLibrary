8ae3078102251da7203b28098e812380cb349ec3
thibo <thibo@LAPTOP-1FMMDL4N>
2021-10-09 11:50:32 +0200

    Add project files.

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public string ISBN13 { get; set; }
+
+        public Book()
+        {
+        }
+
+        public string toString()
+        {
+            return $"Id: {ISBN13}, Title: {Title}, Author: {Author}, PageNumber: {PageNumber}";
+        }
+    }
+}
